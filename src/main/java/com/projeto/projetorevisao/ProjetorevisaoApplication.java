f126a9cb25f3657824daa183033a7de07d0952b0
Aleff Diniz <Aleff_diniz@hotmail.com>
2020-10-07 22:53:01 -0300

    Criação do projeto

---
+++ src/main/java/com/projeto/projetorevisao/ProjetorevisaoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetorevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetorevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetorevisaoApplication.class, args);
+	}
+
+}
